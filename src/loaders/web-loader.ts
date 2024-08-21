f8c234222cec0cabf9d504eb3db95c8243072ed7
julianbopp <julian.bopp@tutanota.com>
2024-08-21 22:21:09 +0200

    Collect all links from a website

--- src/loaders/web-loader.ts
+++ src/loaders/web-loader.ts
@@ -3,6 +3,7 @@ import createDebugMessages from 'debug';
 import { convert } from 'html-to-text';
 import axios from 'axios';
 import md5 from 'md5';
+import * as cheerio from 'cheerio'
 
 import { BaseLoader } from '../interfaces/base-loader.js';
 import { cleanString, truncateCenterString } from '../util/strings.js';
@@ -38,6 +39,10 @@ export class WebLoader extends BaseLoader<{ type: 'WebLoader' }> {
         });
 
         try {
+            let links = await getLinks(this.contentOrUrl)
+            links = links.concat(this.contentOrUrl)
+            console.log(links)
+
             const data = this.isUrl
                 ? (await axios.get<string>(this.contentOrUrl, { responseType: 'document' })).data
                 : this.contentOrUrl;
@@ -67,5 +72,40 @@ export class WebLoader extends BaseLoader<{ type: 'WebLoader' }> {
         } catch (e) {
             this.debug('Could not parse input', this.contentOrUrl, e);
         }
+    }
+}
+async function recursiveDownload
+async function getLinks(url) {
+    try {
+        // Fetch the page content
+        const response = await fetch(url);
+        const html = await response.text();
+
+        // Load the HTML into Cheerio
+        const $ = cheerio.load(html);
+
+        // Select all anchor tags from the page
+        let links = [];
+        $("#rsArticle").find("a").each((index, element) => {
+
+            // Select all href attributes and replace new line characters
+            const link = $(element).attr('href').replace(/(\r\n|\n|\r)/gm, "");
+            if (link && link.trim() !== '') {
+                if (link.includes("html"))
+                    if (!links.includes("www")) {
+                        links.push("www.unibas.ch" + link.trim());
+                    } else {
+                        links.push(link.trim());
+                    }
+            }
+        });
+
+        // Log the links
+
+        // Return the links
+        return links;
+    } catch (err) {
+        console.log('Failed to fetch page: ', err);
+        return []; // Return an empty array in case of error
     }
 }
